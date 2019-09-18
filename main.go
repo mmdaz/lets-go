7a19cde01a004a78e41f0aaa1ba98de3fa6159a8
Muhammad Azhdari <mohammad.azhdari.22@gmail.com>
2019-09-18 14:59:04 +0430

    add log package

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
-import "lets_go/config"
+import (
+	"lets_go/config"
+	"lets_go/log"
+)
 
 func main() {
 	config.Initialize("config/test.yaml")
+	log.Initialize(config.Conf.Log.Level)
+	log.Logger.Info("main started...")
 
 }
